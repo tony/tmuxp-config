07718918062948039e109230b3f499c8052b51e0
Tony Narlock <tony@git-pull.com>
2015-03-23 12:46:20 -0700

    try to make this completely shell-based

--- python/bootstrap_env.sh
+++ python/bootstrap_env.sh
@@ -7,7 +7,51 @@ set -e
 # TODO: bootstrap bootstrap, check for pip, install pystrap
 
 # this step is needed because only shell can source into an environment.
-project_config_dir="$( python bootstrap_env.py $@ --get-project-config)"
+#project_config_dir="$( python bootstrap_env.py $@ --get-project-config)"
+
+
+
+if env | grep -q ^VIRTUAL_ENV=
+then
+    in_virtualenv="in_virtualenv"
+fi
+
+if env | grep -q ^PYENV_VIRTUALENV_INIT=
+then
+    has_pyenv_virtualenv="has_pyenv_virtualenv"
+fi
+
+if env | grep -q ^VIRTUALENVWRAPPER_VIRTUALENV=
+then
+    has_virtualenvwrapper="has_virtualenvwrapper"
+fi
+
+if [ -n "$in_virtualenv" ]; then echo "in virtualenv"; fi
+
+_print_message() {
+    cat <<EOF
+usage: $0 [-v] [-c install_command] [project_name]
+EOF
+}
+
+vflag=off
+while getopts vpc: opt
+do
+    case "$opt" in
+      v)  vflag=on;;
+      p)  project_name="$OPTARG";;
+      c)  install_command="$OPTARG";;
+      \?)		# unknown flag
+         _print_message 
+          exit 1;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+#project_name2=${@:$OPTIND}
+project_name=$@
+echo $project_name
+echo $install_command
 
 # if [ ! -z $PYBOOTSTRAP_DIR ]; then
 #     if [ ! -d $PYBOOTSTRAP_DIR ]; then
@@ -18,7 +62,7 @@ project_config_dir="$( python bootstrap_env.py $@ --get-project-config)"
 #         exit 1
 #     fi
 # fi
-python bootstrap_env.py $@
+#python bootstrap_env.py $@
 
 #echo "$retval"
 
